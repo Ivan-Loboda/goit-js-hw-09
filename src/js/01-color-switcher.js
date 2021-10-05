760fbcd57a680a06c75a23c7be6234dc6f00cd2a
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-05 15:20:49 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
+}
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId;
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => document.querySelector('body').style.backgroundColor = getRandomHexColor(), 1000);
+    btnStop.disabled = false;
+    btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    // e.currentTarget.removeAttribute("disabled");
+});
