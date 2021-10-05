3de6125a9f0cbe3d8bb9dbfd86ab8c6da1530933
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-05 18:22:47 +0300

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import notiflix from 'notiflix';
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const obj = { position, delay };
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) resolve(obj);
+      else reject(obj);
+    }, delay);
+  })
+
+    .then(({ position, delay }) => {
+      notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+
+    .catch(({ position, delay }) => {
+      notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+};
+
+const inputForm = document.querySelector('.form');
+
+inputForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  let delay = parseInt(event.target.elements.delay.value);
+  const step = parseInt(event.target.elements.step.value);
+  const amount = parseInt(event.target.elements.amount.value);
+
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay);
+
+    delay = delay + step;
   }
-}
+};
