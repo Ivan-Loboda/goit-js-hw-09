4691f9735531e6eaf6dcc46e7320be8ca5c87faf
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-05 18:29:55 +0300

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,5 @@
 import notiflix from 'notiflix';
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
